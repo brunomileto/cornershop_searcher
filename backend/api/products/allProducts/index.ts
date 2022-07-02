153213e94429634841dc747cff32b0ed8cc2715a
bruno.mileto <brunomileto@outlook.com>
2022-07-02 17:59:24 -0300

    changes to allow cors

--- backend/api/products/allProducts/index.ts
+++ backend/api/products/allProducts/index.ts
@@ -7,4 +7,21 @@ const allProducts = async (request: Request, response: Response) => {
   return getProductsController.handle(request, response);
 };
 
-export default cors(allProducts);
+const allowCors = (fn) => async (request: Request, response: Response) => {
+  response.setHeader("Access-Control-Allow-Credentials", "true");
+  response.setHeader("Access-Control-Allow-Origin", "*");
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  response.setHeader("Access-Control-Allow-Methods", "POST");
+  response.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+  if (request.method === "OPTIONS") {
+    response.status(200).end();
+    return;
+  }
+  return await fn(request, response);
+};
+
+export default cors(allowCors(allProducts));
--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -3,10 +3,6 @@ import axios from "axios";
 export const api = axios.create({
   baseURL: "https://cornershopper-searcher-api.vercel.app/api",
   headers: {
-    "Access-Control-Allow-Credentials": "true",
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-    "Access-Control-Allow-Headers":
-      "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+    "content-type": "application/json; charset=utf-8",
   },
 });
